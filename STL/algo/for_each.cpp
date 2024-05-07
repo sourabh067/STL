2ae10d6ad1759fe18da4bd28c4db08ac32cf2f5e
Sourabh Biradar <bsourabh067@gmail.com>
2024-05-07 05:36:14 +0530

    for_each.cpp

--- STL/algo/for_each.cpp
+++ STL/algo/for_each.cpp
@@ -12,7 +12,8 @@ int main() {
     // for_each
     
     vector<int> v={2,4,6,8};
-    for_each(v.begin(),v.end(),Double);
+    for_each(v.begin(),v.end(),Double);  // all elements of `v` are run passed as arguments for `Double` function one by one
+    // for_each is a loop
 
     return 0;
 }
